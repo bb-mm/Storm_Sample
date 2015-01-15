ac44323efcad23bb4db4b23e34ee27263509d132
bb-mm <gx9696@126.com>
2015-01-15 17:07:46 +0800

    add local topology for test

--- src/main/java/odata/subway/tokyo/realtime/topology.java
+++ src/main/java/odata/subway/tokyo/realtime/topology.java
@@ -1,5 +1,30 @@
 package odata.subway.tokyo.realtime;
 
-public class topology {
+import odata.subway.tokyo.realtime.bolt.*;
+import odata.subway.tokyo.realtime.spout.*;
+import backtype.storm.LocalCluster;
+import backtype.storm.Config;
+import backtype.storm.StormSubmitter;
+import backtype.storm.topology.TopologyBuilder;
 
+public class topology {
+	public static void main(String args[]) {
+		 TopologyBuilder builder = new TopologyBuilder();
+		 builder.setSpout( "spout", new Alldata_Spout());
+	     builder.setBolt( "calc_bolt", new CalculatorBolt())
+	                .shuffleGrouping("spout");
+	     builder.setBolt("report", new AggregateBolt()).globalGrouping("calc_bolt");
+	     Config conf = new Config();
+	        //conf.put("wordsFile", "OData-test.csv");
+	        conf.setDebug(true);
+	        conf.put(Config.TOPOLOGY_MAX_SPOUT_PENDING, 1);
+	        LocalCluster cluster = new LocalCluster();
+	        cluster.submitTopology("test", conf, builder.createTopology());
+	        //Thread.sleep(1000);
+	        //cluster.shutdown();
+//	        conf.setNumWorkers(5);
+//	        conf.setMaxSpoutPending(5000); 
+//	        StormSubmitter.submitTopology("temp-monitor-topology",conf,builder.createTopology());
+	}
+	
 }
